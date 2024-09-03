c5f9231cf98f2a78dedd71215bf0821b2bab1af9
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2024-09-04 00:13:43 +0530

    added initial message handlers

---
+++ src/store/Store.ts
@@ -0,0 +1,31 @@
+
+
+export type UserId =string;
+export interface Chat {
+    id:string,
+    userId: UserId,
+    name: string,
+    message: string,
+    upvotes: UserId[];
+}
+export abstract class Store {
+    constructor() {
+        
+    }
+
+    initRoom(roomId:string) {
+
+    }
+
+    getChats(room :string,limit: number,offset:number){
+
+    }
+
+    addChat(userId: UserId, name: string,message: string,roomId :string){
+       
+    }
+
+    upvote(userId: UserId, room :string,chatId: string){
+
+    }
+}
